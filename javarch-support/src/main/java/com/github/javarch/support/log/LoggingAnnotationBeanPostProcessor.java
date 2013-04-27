385452e5eff4a1792ab6aae4228557b8d4a77874
Lucas Oliveira <luksrn@gmail.com>
2013-04-27 15:40:19 -0300

    Correção no Post Bean Processor

--- javarch-support/src/main/java/com/github/javarch/support/log/LoggingAnnotationBeanPostProcessor.java
+++ javarch-support/src/main/java/com/github/javarch/support/log/LoggingAnnotationBeanPostProcessor.java
@@ -1,23 +1,33 @@
 package com.github.javarch.support.log;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.support.MergedBeanDefinitionPostProcessor;
-import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
-@Component
-public class LoggingAnnotationBeanPostProcessor implements MergedBeanDefinitionPostProcessor {
-	
-    public void postProcessMergedBeanDefinition(RootBeanDefinition rootBeanDefinition, Class beanType, String beanName) {
-    }
+import com.github.javarch.support.annotations.FieldAnnotationUtils;
 
+/**
+ * http://www.rimple.com/tech/2011/2/21/spring-corner-what-are-those-post-processor-beans-anyway.html
+ * 
+ * @author Lucas Oliveira <luksrn@gmail.com>
+ *
+ */
+@Component
+public class LoggingAnnotationBeanPostProcessor implements BeanPostProcessor {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(LoggingAnnotationBeanPostProcessor.class);
+	
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        Field[] fields = bean.getClass().getDeclaredFields();
+    	
+    	LOG.trace("Searching @Logging annotation into {}", beanName );
+        List<Field> fields = FieldAnnotationUtils.findFieldsWithAnnotation( bean.getClass() , Logging.class );
         for (Field field : fields) {
-            if (field.getAnnotation(Logging.class) != null) {
+            if ( field.getAnnotation(Logging.class) != null ) {
+            	LOG.debug("Bean {} has @Logging annotation. Injecting Logger...", beanName );
                 injectLogger(bean, field);
             }
         }
@@ -26,8 +36,7 @@ public class LoggingAnnotationBeanPostProcessor implements MergedBeanDefinitionP
 
     private void injectLogger(Object bean, Field field) {
         ReflectionUtils.makeAccessible(field);
-        ReflectionUtils.setField(field, bean,
-                                 LoggerFactory.getLogger(field.getDeclaringClass()));
+        ReflectionUtils.setField(field, bean, LoggerFactory.getLogger(field.getDeclaringClass()));
     }
 
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
