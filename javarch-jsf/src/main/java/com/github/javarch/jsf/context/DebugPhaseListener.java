af76ce6dece7657e3799d8579d59ee39b6501787
Lucas Oliveira <luksrn@gmail.com>
2013-05-02 19:58:05 -0300

    Licença em códigos

--- javarch-jsf/src/main/java/com/github/javarch/jsf/context/DebugPhaseListener.java
+++ javarch-jsf/src/main/java/com/github/javarch/jsf/context/DebugPhaseListener.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2011 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy of
+* the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations under
+* the License.
+*/
 package com.github.javarch.jsf.context;
 
 
@@ -33,16 +48,17 @@ public class DebugPhaseListener implements PhaseListener {
 	
 	private void debugPhase(String phase, PhaseEvent phaseEvent) {
 		 
-		PhaseId phaseId = phaseEvent.getPhaseId();
-		UIViewRoot uiViewRoot = phaseEvent.getFacesContext().getViewRoot();
-
-		String viewId = null;
-		if (uiViewRoot != null) {
-			viewId = uiViewRoot.getViewId();
+		if( logger.isDebugEnabled() ){
+			PhaseId phaseId = phaseEvent.getPhaseId();
+			UIViewRoot uiViewRoot = phaseEvent.getFacesContext().getViewRoot();
+	
+			String viewId = null;
+			if (uiViewRoot != null) {
+				viewId = uiViewRoot.getViewId();
+			}
+	
+			logger.debug( phase + " phaseId=[{0}] viewId=[{1}]", phaseId.toString(), viewId);
 		}
-
-		logger.debug( phase + " phaseId=[{0}] viewId=[{1}]", phaseId.toString(), viewId);
-		
 		
 	}
 }
