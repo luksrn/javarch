821e2116c039b7342ff7eb726d37bd9285e558a1
Lucas Farias de Oliveira <luksrn@gmail.com>
2013-06-02 10:15:24 -0300

    Adição do powermock para realizar o teste do JSF, adição também de dependências da EL que estavam causando erro nos testes com JSF

--- javarch-jsf/src/test/java/com/github/javarch/jsf/MessageContextTest.java
+++ javarch-jsf/src/test/java/com/github/javarch/jsf/MessageContextTest.java
@@ -1,6 +1,8 @@
 package com.github.javarch.jsf;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,32 +11,42 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.github.javarch.jsf.context.MessageContext;
 
-import static org.mockito.Mockito.*;
-
-@Ignore
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { FacesContext.class })
 public class MessageContextTest {
 	
+	@Mock
+	private FacesContext facesContext;
 
 	private MessageContext messageContext;
 	 
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() throws Exception {
 		messageContext = new MessageContext();
-	 	FacesContext facesContext = mock(FacesContext.class);	
-		when(facesContext.getMessageList()).thenReturn( new ArrayList<FacesMessage>() );
+
+        PowerMockito.mockStatic(FacesContext.class);
+        PowerMockito.doReturn(this.facesContext).when(FacesContext.class, "getCurrentInstance");
+        
+		Mockito.when(this.facesContext.getMessageList()).thenReturn( new ArrayList<FacesMessage>() );
 		
-	//	messageContext.setFacesContext(facesContext);
+		 
 	}
 
 	@Test
 	public void test() {
-		List<FacesMessage> mensagens = messageContext.getMessages();
+		//List<FacesMessage> mensagens = messageContext.getMessages();
+		List<FacesMessage> mensagens = facesContext.getMessageList();
 		assertNotNull(mensagens);
 	}
 
